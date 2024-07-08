e1ff0dfab19545c0acbb63ac68eeb2f51b5fb3aa
Thanh Duong <thanhduonghd114@gmail.com>
2024-07-08 16:52:21 +0300

    update: add switch logic for resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,34 +1,78 @@
+import React, { useState } from "react";
+
 const Resume = () => {
+  const [fields, setFields] = useState('Experience');
+
   const Experience = () => {
     return (
-      <></>
-    )
-  }
-  const handleExperience = () => {
-    return <Experience />
-  }
+      <div>
+        <h2 className="font-bold text-xl">My experience</h2>
+      </div>
+    );
+  };
+
+  const Education = () => {
+    return (
+      <div>
+        <h2 className="font-bold text-3xl">Education</h2>
+      </div>
+    );
+  };
+
+  const Skills = () => {
+    return (
+      <div>
+        <h2 className="font-bold text-3xl">Skills</h2>
+      </div>
+    );
+  };
+
+  const AboutMe = () => {
+    return (
+      <div>
+        <h2 className="font-bold text-3xl">About me</h2>
+      </div>
+    );
+  };
+
+  const components = {
+    Experience,
+    Education,
+    Skills,
+    AboutMe
+  };
+
+  const handleSwitch = (text) => () => {
+    setFields(text);
+  };
+
+  const ComponentToRender = components[fields];
+
   return (
     <div className="py-24 sm:py-32">
       <div className="flex flex-col sm:flex-row mx-auto mt-10 max-w-2xl rounded-3xl ring-1 lg:max-w-none lg:mx-0">
-          <div className="p-8 lg:p-12 w-full sm:w-1/3 ">
-            <h2 className="font-bold text-3xl">Why hire me?</h2>
-            <p className="mt-6 tracking-tight leading-7 text-base text-gray-200">I'm a driven and talented developer who is committed to pushing the boundaries of what's possible in technology field </p>
-            <div className="mt-10 flex items-center gap-x-3">
-              <h4 className="flex-none text-sm font-semibold leading-6 text-gray-200">Fields</h4>
-              <div class="h-px flex-auto bg-gradient-to-r from-indigo-500 via-slate-500 to-purple-500"></div>
-            </div>
-            <div className="mt-6">
-              <button type="button" onClick={handleExperience} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">Experience</button>
-              <button type="button" onClick={handleExperience} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">Education</button>
-              <button type="button" onClick={handleExperience} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">Skills</button>
-              <button type="button" onClick={handleExperience} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">About me</button>
-            </div>
+        <div className="p-8 lg:p-12 w-full sm:w-1/3 ">
+          <h2 className="font-bold text-3xl">Why hire me?</h2>
+          <p className="mt-6 tracking-tight leading-7 text-base text-gray-200">
+            I'm a driven and talented developer who is committed to pushing the boundaries of what's possible in technology field
+          </p>
+          <div className="mt-10 flex items-center gap-x-3">
+            <h4 className="flex-none text-sm font-semibold leading-6 text-gray-200">Fields</h4>
+            <div className="h-px flex-auto bg-gradient-to-r from-indigo-500 via-slate-500 to-purple-500"></div>
           </div>
-          <div className="w-full sm:w-2/3">My Experience</div>
+          <div className="mt-6">
+            <button type="button" onClick={handleSwitch('Experience')} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">Experience</button>
+            <button type="button" onClick={handleSwitch('Education')} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">Education</button>
+            <button type="button" onClick={handleSwitch('Skills')} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">Skills</button>
+            <button type="button" onClick={handleSwitch('AboutMe')} className="my-4 rounded-md w-full bg-indigo-300 h-10 text-black hover:bg-sky-700">About me</button>
+          </div>
+        </div>
+        <div className="w-full sm:w-2/3 p-8 lg:p-12">
+          <ComponentToRender />
+        </div>
       </div>
     </div>
-    
-  )
+  );
 };
 
 export default Resume;
