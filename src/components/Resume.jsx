9bb24cf157ce11c98fafe3c102ea6ba30488455f
Thanh Duong <thanhduonghd114@gmail.com>
2024-07-11 18:14:23 +0300

    update: finish experience field in resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,18 +4,18 @@ import {More} from "../assets/index";
 const Resume = () => {
   const [fields, setFields] = useState('Experience');
   const Experience = () => {
-    const [visible, setVisible] = useState(true);
+    const [visibleFor1, setVisibleFor1] = useState(true);
+    const [visibleFor2, setVisibleFor2] = useState(true);
+    const [visibleFor3, setVisibleFor3] = useState(true);
 
-    const toggleVisible = () => {
-      setVisible(!visible);
-    }
+    
     return (
       <div>
         <h2 className="font-bold text-xl">My experience</h2>
         <div className="mt-8 lg:mt-6 lg:w-full lg:max-w-4xl lg:flex-shrink-0 min-w-2xl overflow-y-scroll max-h-none lg:max-h-[29rem]">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="bg-indigo-200 rounded-2xl text-black px-6 pt-6 pb-14 ring-3 ring-inset ring-red-900 relative">
-              { visible ? (
+            <div className="bg-indigo-200 rounded-2xl text-black px-6 pt-6 pb-14 ring-3 ring-inset ring-red-900 relative min-h-[17rem] xl:min-h-[18rem] sm:min-h-[22rem] md:min-h-[19rem]">
+              { visibleFor1 ? (
                 <>
                   <h3 className="font-bold text-2xl">Software Developer</h3>
                   <p class=" flex items-baseline justify-start gap-x-0.5">
@@ -29,7 +29,7 @@ const Resume = () => {
                   <p className="mt-6">Worked on an <span className="text-indigo-500 underline hover:text-indigo-800"><a href="https://drive.google.com/file/d/10Ui35zvfikHiTHBP_OKcnFzyouDFWtJr/view?usp=sharing">Launch Pad</a></span> project helping the end-user use to select the correct solution component for their daily work with ease. </p>
                   
                   <p className="inline-block absolute bottom-6 right-16 text-[15px]">Know more</p>
-                  <button type="button" className="rounded-md absolute bottom-3 right-3 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={toggleVisible}><img src={More} className="w-12 h-12"></img></button>
+                  <button type="button" className="rounded-md absolute bottom-3 right-3 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={() => {setVisibleFor1(!visibleFor1)}}><img src={More} className="w-12 h-12"></img></button>
                 </>
               ):
               <>
@@ -41,13 +41,13 @@ const Resume = () => {
 
                 </ur>
                 <p className="inline-block absolute bottom-6 right-16 text-[15px]">Go back</p>
-                <button type="button" className="rounded-md absolute bottom-3 right-3 rotate-180 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={toggleVisible}><img src={More} className="w-12 h-12"></img></button>
+                <button type="button" className="rounded-md absolute bottom-3 right-3 rotate-180 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={() => {setVisibleFor1(!visibleFor1)}}><img src={More} className="w-12 h-12"></img></button>
               </>}
             </div>
-            <div className="bg-indigo-200 rounded-2xl text-black px-6 pt-6 pb-14 ring-3 ring-inset ring-red-900 relative">
-              { visible ? (
+            <div className="bg-indigo-200 rounded-2xl text-black px-6 pt-6 pb-14 ring-3 ring-inset ring-red-900 relative min-h-[17rem] xl:min-h-[18rem] sm:min-h-[22rem] md:min-h-[19rem]">
+              { visibleFor2 ? (
                 <>
-                  <h3 className="font-bold text-2xl">Software Developer</h3>
+                  <h3 className="font-bold text-2xl">Teaching Assistant</h3>
                   <p class=" flex items-baseline justify-start gap-x-0.5">
                   <span class="text-[13px] font-bold tracking-tight text-gray-900">2024</span>
                   <span class="text-[11px] font-semibold leading-6 tracking-wide text-gray-600">Jan</span> 
@@ -55,11 +55,11 @@ const Resume = () => {
                   <span class="text-[13px] font-bold tracking-tight text-gray-900">2024</span>
                   <span class="text-[11px] font-semibold leading-6 tracking-wide text-gray-600">May</span> 
                   </p>
-                  <p className="mt-1 text-sm max-w-45">Triplan Company Collabrating With LUT University - Capstone Project</p>
-                  <p className="mt-6">Worked on an <span className="text-indigo-500 underline hover:text-indigo-800"><a href="https://drive.google.com/file/d/10Ui35zvfikHiTHBP_OKcnFzyouDFWtJr/view?usp=sharing">Launch Pad</a></span> project helping the end-user use to select the correct solution component for their daily work with ease. </p>
+                  <p className="mt-1 text-sm max-w-45">LUT University</p>
+                  <p className="mt-6">Assisting  two other professors to conduct Basics of Database Systems Course with more than <span className="text-red-400">100</span> students </p>
                   
                   <p className="inline-block absolute bottom-6 right-16 text-[15px]">Know more</p>
-                  <button type="button" className="rounded-md absolute bottom-3 right-3 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={toggleVisible}><img src={More} className="w-12 h-12"></img></button>
+                  <button type="button" className="rounded-md absolute bottom-3 right-3 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={() => {setVisibleFor2(!visibleFor2)}}><img src={More} className="w-12 h-12"></img></button>
                 </>
               ):
               <>
@@ -71,25 +71,22 @@ const Resume = () => {
 
                 </ur>
                 <p className="inline-block absolute bottom-6 right-16 text-[15px]">Go back</p>
-                <button type="button" className="rounded-md absolute bottom-3 right-3 rotate-180 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={toggleVisible}><img src={More} className="w-12 h-12"></img></button>
+                <button type="button" className="rounded-md absolute bottom-3 right-3 rotate-180 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={() => {setVisibleFor2(!visibleFor2)}}><img src={More} className="w-12 h-12"></img></button>
               </>}
             </div>
-            <div className="bg-indigo-200 rounded-2xl text-black px-6 pt-6 pb-14 ring-3 ring-inset ring-red-900 relative">
-              { visible ? (
+            <div className="bg-indigo-200 rounded-2xl text-black px-6 pt-6 pb-14 ring-3 ring-inset ring-red-900 relative min-h-[17rem] xl:min-h-[18rem] sm:min-h-[22rem] md:min-h-[19rem]">
+              { visibleFor3 ? (
                 <>
-                  <h3 className="font-bold text-2xl">Software Developer</h3>
+                  <h3 className="font-bold text-2xl">Competitive Programmer</h3>
                   <p class=" flex items-baseline justify-start gap-x-0.5">
-                  <span class="text-[13px] font-bold tracking-tight text-gray-900">2024</span>
-                  <span class="text-[11px] font-semibold leading-6 tracking-wide text-gray-600">Jan</span> 
+                  <span class="text-[13px] font-bold tracking-tight text-gray-900">2019</span>
                   <span class="mx-2 text-lg font-bold tracking-tight text-gray-900">-</span>
-                  <span class="text-[13px] font-bold tracking-tight text-gray-900">2024</span>
-                  <span class="text-[11px] font-semibold leading-6 tracking-wide text-gray-600">May</span> 
+                  <span class="text-[13px] font-bold tracking-tight text-gray-900">Present</span>
                   </p>
-                  <p className="mt-1 text-sm max-w-45">Triplan Company Collabrating With LUT University - Capstone Project</p>
-                  <p className="mt-6">Worked on an <span className="text-indigo-500 underline hover:text-indigo-800"><a href="https://drive.google.com/file/d/10Ui35zvfikHiTHBP_OKcnFzyouDFWtJr/view?usp=sharing">Launch Pad</a></span> project helping the end-user use to select the correct solution component for their daily work with ease. </p>
+                  <p className="mt-6">Solved problems on <span className="text-indigo-500 underline hover:text-indigo-800"><a href="https://leetcode.com/u/alexha11/">LeetCode</a></span>, <span className="text-indigo-500 underline hover:text-indigo-800"><a href="http://lequydon.ntucoder.net/Coder/Details/a519Duong">LQDcoder</a></span>, <span className="text-indigo-500 underline hover:text-indigo-800"><a href="https://lqdoj.edu.vn/user">LQDoj</a></span> using C++, and Python. For three years, I have solved more than <span className="text-red-400">400</span> problems and achieved many prizes in VietNam.</p>
                   
                   <p className="inline-block absolute bottom-6 right-16 text-[15px]">Know more</p>
-                  <button type="button" className="rounded-md absolute bottom-3 right-3 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={toggleVisible}><img src={More} className="w-12 h-12"></img></button>
+                  <button type="button" className="rounded-md absolute bottom-3 right-3 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={() => {setVisibleFor3(!visibleFor3)}}><img src={More} className="w-12 h-12"></img></button>
                 </>
               ):
               <>
@@ -101,7 +98,7 @@ const Resume = () => {
 
                 </ur>
                 <p className="inline-block absolute bottom-6 right-16 text-[15px]">Go back</p>
-                <button type="button" className="rounded-md absolute bottom-3 right-3 rotate-180 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={toggleVisible}><img src={More} className="w-12 h-12"></img></button>
+                <button type="button" className="rounded-md absolute bottom-3 right-3 rotate-180 hover:bg-gray-200 hover:scale-105 transition-transform duration-300" onClick={() => {setVisibleFor3(!visibleFor3)}}><img src={More} className="w-12 h-12"></img></button>
               </>}
             </div>
           </div>
